df236fec79a449e8a50a02c42004f3ba158451b6
Chaitanya Krish Kanneganti <chaitu1617@gmail.com>
2023-02-21 16:57:11 +1100

    Initial Commit

---
+++ src/main/kotlin/com/example/identitymanagerkotlin/IdentityManagerKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.identitymanagerkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IdentityManagerKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<IdentityManagerKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/identitymanagerkotlin/IdentityManagerKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.identitymanagerkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IdentityManagerKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
